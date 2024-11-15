4902cd542f1959446ee2a069bccbf83ef07d5ae3
TagadaZim <tagadazim@eleves.polytechnique.fr>
2024-11-15 15:16:05 +0100

    fix bugs

--- run_onchange_before_1.3-zsh-setup.sh
+++ run_onchange_before_1.3-zsh-setup.sh
@@ -4,7 +4,7 @@ sudo -v || exit
 cd $HOME
 
 # folder required for ssh-agent plugin to work
-mkdir $HOME/.ssh
+mkdir $HOME/.ssh 2>/dev/null
 
 # changing default shell without requesting user
 # password (use sudo rights already present)
@@ -22,18 +22,19 @@ fi
 # checking if plugin already are present before cloning
 # the repos
 ## zsh-autosuggestions
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"]; then
+if ! [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 ## zsh-syntax-highlighting
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"]; then
+if ! [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 ## powerlevel10k theme
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"]; then
+if ! [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 # since we managed the dot files by ourselves we
 # do not want oh-my-zsh to replace a already existent .zshrc
 mv .zshrc.pre-oh-my-zsh .zshrc 2>/dev/null
+true
