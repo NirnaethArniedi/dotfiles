009f8998216e0d96de39f5c5883bce2f5c25c0ce
NirnaethArniedi <augustin.lenormand_github@polytechnique.org>
2025-02-27 19:43:36 +0100

    fix(lsp): lsp again...

--- dot_config/nvim/lua/plugins/lsp_config.lua
+++ dot_config/nvim/lua/plugins/lsp_config.lua
@@ -5,6 +5,7 @@ end
 return {
   -- change nvim-lspconfig options
   "neovim/nvim-lspconfig",
+
   dependencies = {
     "williamboman/mason.nvim",
   },
@@ -33,25 +34,24 @@ return {
     -- ...
   end,
 
-  setup = function()
-    -- Other LSP setup code here...
-
-    -- Add the Ruff-specific configuration
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client == nil then
-          return
-        end
-        if client.name == "ruff" then
-          -- Disable hover in favor of Pyright
-          client.server_capabilities.hoverProvider = false
-        end
-      end,
-      desc = "LSP: Disable hover capability from Ruff",
-    })
-
-    -- Other LSP configurations...
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          capabilities = {
+            textDocument = {
+              publishDiagnostics = {
+                tagSupport = {
+                  valueSet = { 2 },
+                },
+              },
+            },
+          },
+          --       mason = false,
+          -- autostart = false,
+        },
+      },
+    },
+  },
 }
