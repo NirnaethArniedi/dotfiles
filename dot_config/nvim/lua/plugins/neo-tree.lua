fbd11f0001918927b321ebb400ef7cb7b616c0b7
NirnaethArniedi <augustin.lenormand_github@polytechnique.org>
2025-03-05 14:46:39 +0100

    fix(neo-tree.lua): fix new window opening for first file
    https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1703

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,10 +1,13 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = function(_, opts)
-    opts.window = opts.window or {}
-    opts.window.mappings = opts.window.mappings or {}
-    opts.window.mappings["<cr>"] = "open_drop"
-
-    return opts
-  end,
+  -- This configuration ensures that when opening a file, it reuses existing windows
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.window = opts.window or {}
+      opts.window.mappings = opts.window.mappings or {}
+      opts.window.mappings["<cr>"] = "open_drop"
+      opts.open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline", "edgy", "toggleterm" }
+      return opts
+    end,
+  },
 }
