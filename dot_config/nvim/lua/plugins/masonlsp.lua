9b78bb4d5e7d9a8395c9953a380a3766f3ca66c3
TagadaZim <tagadazim@eleves.polytechnique.fr>
2025-03-16 19:40:42 +0100

    aider + ruff lsp

--- dot_config/nvim/lua/plugins/masonlsp.lua
+++ dot_config/nvim/lua/plugins/masonlsp.lua
@@ -5,8 +5,8 @@ return {
   "williamboman/mason-lspconfig.nvim",
 
   config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "ruff_lsp" },
-    })
+    --    require("mason-lspconfig").setup({
+    --      ensure_installed = { "ruff_lsp" },
+    --    })
   end,
 }
